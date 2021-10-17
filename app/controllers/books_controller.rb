490056c92126e9167742754f40b1aef139783c6e
EC2 Default User <ec2-user@ip-172-31-91-73.ec2.internal>
2021-10-17 04:06:33 +0000

    7th

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,11 +4,13 @@ class BooksController < ApplicationController
     @book_new = Book.new
     @book = Book.find(params[:id])
     @user = @book.user
+    @book_comment = BookComment.new
   end
 
   def index
     @book = Book.new
     @books = Book.all
+
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
 
 
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
-
+  resources :books do
+    resources :book_comments, only: [:create,:destroy]
+    resource :favorites, only: [:create,:destroy]
+  end
 
 end
